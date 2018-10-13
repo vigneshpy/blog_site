0543985efb66ebe32e0d006843c5ff90a9290c20
vicky <vickypotter2516@gmail.com>
2018-10-13 21:14:04 +0530

    runned collect static

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 t=os.path.join(BASE_DIR,'templates')
-# s=os.path.join(BASE_DIR,'static')
+s=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS=[s,]
+STATICFILES_DIRS=[s,]
+
+STATIC_ROOT = '/assets/'
 
 
