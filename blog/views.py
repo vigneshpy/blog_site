b741aece7ca91cf99c64a2e3cd65e7493418e941
vicky <vickypotter2516@gmail.com>
2018-10-19 08:58:54 +0530

    added post

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,8 @@ from blog.models import Post
 
 # Create your views here.
 def index(request):
-	post=Post()
-	title=post.title
-	print(title)
-		
-
+	post=Post.objects.all()
+	
+	
 
 	return render(request,'blog/index.html',{'post':post})
