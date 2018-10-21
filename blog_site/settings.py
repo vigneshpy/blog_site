0045a26e1331b58830636124c51ed65c6ffacd49
vicky <vickypotter2516@gmail.com>
2018-10-21 23:21:59 +0530

    attempted to deploy on heroku

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -24,10 +24,10 @@ s=os.path.join(BASE_DIR,'blog/static')
 SECRET_KEY = 'y@im)0+jtg&&53-3i8_8q-9#zzdbl8dc4a78%4k&tgc$*cn&*@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['192.168.43.102']
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,12 +76,13 @@ WSGI_APPLICATION = 'blog_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+ import dj_database_url
+
+  DATABASES = {
+      'default': dj_database_url.config(
+          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+      )
+  }
 
 
 # Password validation
